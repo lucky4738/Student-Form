ce7b04da4c385e4ed02ee781b295644f02767525
lucky4738 <112933131+lucky4738@users.noreply.github.com>
2022-10-03 09:48:08 +0530

    Add files via upload

---
+++ Student.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Student.aspx.cs" Inherits="Music_Store.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+   
+    
+    <h1>STUDENT FORM</h1>
+
+    <table style="width: 60%;" border="1">
+        <tr>
+            <td>Student Name:</td>
+            <td> <asp:TextBox ID="txtName" runat="server" Width="300px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtName" ErrorMessage="Enter  Name" ForeColor="Red"></asp:RequiredFieldValidator>
+                <br /></td>
+           
+        </tr>
+        <tr>
+            <td>Roll No:</td>
+            <td><asp:TextBox ID="txtRollNo" runat="server" Width="300px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtRollNo" ErrorMessage="Enter Roll No" ForeColor="Red"></asp:RequiredFieldValidator>
+                <br /></td>
+            
+        </tr>
+        <tr>
+            <td style="height: 26px">DOB:</td>
+            <td style="height: 26px"><asp:TextBox ID="txtDOB" runat="server" Width="300px"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtDOB" ErrorMessage="Enter DOB" ForeColor="Red"></asp:RequiredFieldValidator>
+                <br /> </td>
+            
+        </tr>
+         <tr>
+                <td>Class:</td>
+                <td><asp:TextBox ID="txtClass" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="txtClass" ErrorMessage="Enter Class" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+                
+            </tr>
+            <tr>
+                <td>Section:</td>
+                <td> <asp:TextBox ID="txtSection" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ControlToValidate="txtSection" ErrorMessage="Enter Section" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+               
+            </tr>
+            <tr>
+                <td>Class Teacher:</td>
+                <td><asp:TextBox ID="txtClassTeacher" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator6" runat="server" ControlToValidate="txtClassTeacher" ErrorMessage="Enter Class Teacher Name" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+               
+            </tr>
+        <tr>
+                <td>Address:</td>
+                <td><asp:TextBox ID="txtAddress" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator7" runat="server" ControlToValidate="txtAddress" ErrorMessage="Enter Address" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+                
+            </tr>
+            <tr>
+                <td>Mobile No:</td>
+                <td><asp:TextBox ID="txtMobileNo" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator8" runat="server" ControlToValidate="txtMobileNo" ErrorMessage="Enter Mobile No" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+               
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><asp:TextBox ID="txtEmail" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator9" runat="server" ControlToValidate="txtEmail" ErrorMessage="Enter Email" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+               
+            </tr>
+        <tr>
+                <td>Date of Admission:</td>
+                <td> <asp:TextBox ID="txtDateOfAdmission" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator10" runat="server" ControlToValidate="txtDateOfAdmission" ErrorMessage="Enter Date of Admission" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+                
+            </tr>
+            <tr>
+                <td>Adhaar No:
+                </td>
+                <td> <asp:TextBox ID="txtAdhaar" runat="server" Width="300px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator11" runat="server" ControlToValidate="txtAdhaar" ErrorMessage="Enter Adhaar No" ForeColor="Red"></asp:RequiredFieldValidator>
+                    <br /></td>
+             </tr>
+        
+            <tr>
+               
+                <td colspan="2"><asp:Button ID="btnSubmit" runat="server" Text="Submit Data" OnClick="btnSubmit_Click" /></td>
+                    
+                    </tr>
+        
+                 <tr>
+               
+                <td colspan="2"><asp:Button ID="btnShow" runat="server" Text="Show Data" OnClick="btnShow_Click" /></td>
+                    
+    </tr>
+   </table>
+    <br />
+    <br />
+    <asp:DataGrid ID="dgrd" runat="server">
+        <Columns>
+        </Columns>
+    </asp:DataGrid>
+    </asp:Content>
---
+++ Student.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services.Protocols;
+
+namespace Music_Store
+{
+    public partial class About : Page
+    {
+        DataTable gdt;
+       private DataTable GenerateTable()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Student Name");
+            dt.Columns.Add("Roll No");
+            dt.Columns.Add("DOB");
+            dt.Columns.Add("Class");
+            dt.Columns.Add("Section");
+            dt.Columns.Add("Class Teacher");
+            dt.Columns.Add("Address");
+            dt.Columns.Add("Mobile No");
+            dt.Columns.Add("Email");
+            dt.Columns.Add("Date of Admission");
+            dt.Columns.Add("Adhaar No");
+            return dt;
+        }
+       
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gdt = GenerateTable();
+        }
+        private void SaveGrid()
+        {
+            DataTable gdt;
+            if(ViewState["dt"]==null)
+            {
+                gdt = GenerateTable();
+
+            }else
+                gdt=((DataTable)ViewState["dt"]);
+
+            object[] row = new object[11];
+            row[0] = Convert.ToString(txtName.Text);
+            row[1] = Convert.ToString(txtRollNo.Text);
+            row[2] = Convert.ToString(txtDOB.Text);
+            row[3] = Convert.ToString(txtClass.Text);
+            row[4] = Convert.ToString(txtSection.Text);
+            row[5] = Convert.ToString(txtClassTeacher.Text);
+            row[6] = Convert.ToString(txtAddress.Text);
+            row[7] = Convert.ToString(txtMobileNo.Text);
+            row[8] = Convert.ToString(txtEmail.Text);
+            row[9] = Convert.ToString(txtDateOfAdmission.Text);
+            row[10] = Convert.ToString(txtAdhaar.Text);
+
+
+
+            gdt.Rows.Add(row);
+
+            ViewState["dt"] = gdt;
+            dgrd.DataSource = gdt;
+            dgrd.DataBind();
+
+
+        }
+        private void WriteToFile()
+        {
+            FileInfo fl = new FileInfo("D:\\Espire\\C#\\Student\\progfile.txt");
+            
+            FileStream fs = fl.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite); 
+
+            object[] obj = new object[11];
+            obj[0] = Convert.ToString(txtName.Text);
+            obj[1] = Convert.ToString(txtRollNo.Text);
+            obj[2] = Convert.ToString(txtDOB.Text);
+            obj[3] = Convert.ToString(txtClass.Text);
+            obj[4] = Convert.ToString(txtSection.Text);
+            obj[5] = Convert.ToString(txtClassTeacher.Text);
+            obj[6] = Convert.ToString(txtAddress.Text);
+            obj[7] = Convert.ToString(txtMobileNo.Text);
+            obj[8] = Convert.ToString(txtEmail.Text);
+            obj[9] = Convert.ToString(txtDateOfAdmission.Text);
+            obj[10] = Convert.ToString(txtAdhaar.Text);
+
+            StreamWriter s = new StreamWriter(fs);
+            s.WriteLine(obj[0].ToString());
+            s.WriteLine(obj[1].ToString());
+            s.WriteLine(obj[2].ToString());
+            s.WriteLine(obj[3].ToString());
+            s.WriteLine(obj[4].ToString());
+            s.WriteLine(obj[5].ToString());
+            s.WriteLine(obj[6].ToString());
+            s.WriteLine(obj[7].ToString());
+            s.WriteLine(obj[8].ToString());
+            s.WriteLine(obj[9].ToString());
+            s.WriteLine(obj[10].ToString());
+            s.Close();
+
+        }
+       /* private void ReadDataFromFile(string fileName)
+        {
+            FileInfo fl = new FileInfo("D:\\Espire\\C#\\Student\\progfile.txt");
+
+            FileStream fs = fl.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            StreamReader sr = new StreamReader(fs);
+            while(!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+                Response.Write(s);
+            }
+            sr.Close();
+        }*/
+
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WriteToFile();
+            }
+            catch(Exception a)
+            {
+                Response.Write(a.Message);
+            }
+            finally
+            {
+                Response.Write("finally Block");
+            }
+
+        }
+
+        
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                object[] row = new object[11];
+                row[0] = Convert.ToString(txtName.Text);
+                row[1] = Convert.ToString(txtRollNo.Text);
+                row[2] = Convert.ToString(txtDOB.Text);
+                row[3] = Convert.ToString(txtClass.Text);
+                row[4] = Convert.ToString(txtSection.Text);
+                row[5] = Convert.ToString(txtClassTeacher.Text);
+                row[6] = Convert.ToString(txtAddress.Text);
+                row[7] = Convert.ToString(txtMobileNo.Text);
+                row[8] = Convert.ToString(txtEmail.Text);
+                row[9] = Convert.ToString(txtDateOfAdmission.Text);
+                row[10] = Convert.ToString(txtAdhaar.Text);
+
+
+
+                gdt.Rows.Add(row);
+
+                dgrd.DataSource = gdt;
+                dgrd.DataBind();
+            }
+            catch(Exception a)
+            {
+                Response.Write(a.Message);
+               
+            }
+            finally
+            {
+                Response.Write("finally Block");
+            }
+
+        }
+
+       
+    }
+}
